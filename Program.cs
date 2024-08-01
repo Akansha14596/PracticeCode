5dba6a919ebce2f711d92c2620eac9e52e00e38e
Akansha Mittal <akansha.goel@sopratseria.com>
2024-08-01 11:48:00 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+class Program
+{
+   public static void Main(string[] args)
+   {
+        Console.Write("Enter a character");
+        char character = Console.ReadKey().KeyChar;
+     
+    }
+}
